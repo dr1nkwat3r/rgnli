44925675531d4d4857253bac4846c1f508bdd5bf
Regina Li <rgnahli@gmail.com>
2024-09-10 13:39:35 -0400

    first

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aaaaaaaEdit <code>src/App.js</code> and save to reload.
+          baaaaaaaEdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
